6ecc4abea89fc2e6b1f4e9e2494d730ad7bb7546
indiu6 <hewas6@gmail.com>
2021-01-23 15:06:35 -0500

    done createAction

--- src/store.js
+++ src/store.js
@@ -1,29 +1,15 @@
 import { createStore } from 'redux';
+import { createAction } from '@reduxjs/toolkit';
 
-const ADD = 'ADD';
-const DELETE = 'DELETE';
-
-const addToDo = (text) => {
-  return {
-    type: ADD,
-    text,
-    id: Date.now(),
-  };
-};
-
-const deleteToDo = (id) => {
-  return {
-    type: DELETE,
-    id,
-  };
-};
+const addToDo = createAction('ADD');
+const deleteToDo = createAction('DELETE');
 
 const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: action.id }, ...state];
-    case DELETE:
-      return state.filter((toDo) => toDo.id !== action.id);
+    case addToDo.type:
+      return [{ text: action.payload, id: action.Date.now() }, ...state];
+    case deleteToDo.type:
+      return state.filter((toDo) => toDo.id !== action.payload);
     default:
       return state;
   }
@@ -37,3 +23,23 @@ export const actionCreators = {
 };
 
 export default store;
+
+//todo Replaced by createAction
+
+// const ADD = 'ADD';
+// const DELETE = 'DELETE';
+
+// const addToDo = (text) => {
+//   return {
+//     type: ADD,
+//     text,
+//     id: Date.now(),
+//   };
+// };
+
+// const deleteToDo = (id) => {
+//   return {
+//     type: DELETE,
+//     id,
+//   };
+// };
