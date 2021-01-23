062ef4b9df80c0b9e8ca9647338d286732668ced
indiu6 <hewas6@gmail.com>
2021-01-23 16:34:38 -0500

    done createSlice it's AMAZING

--- src/store.js
+++ src/store.js
@@ -1,28 +1,29 @@
 // import { createStore } from 'redux';
-import { configureStore, createAction, createReducer } from '@reduxjs/toolkit';
+import { configureStore, createSlice } from '@reduxjs/toolkit';
 
-const addToDo = createAction('ADD');
-const deleteToDo = createAction('DELETE');
-
-//todo now it's OK to mutate the state by 'immer'
-//* 2 options: mutate the state OR return new state, but not both
-const reducer = createReducer([], {
-  [addToDo]: (state, action) => {
-    state.push({ text: action.payload, id: Date.now() }); // mutate the state
+const toDos = createSlice({
+  name: 'toDosReducer',
+  initialState: [],
+  reducers: {
+    add: (state, action) => {
+      state.push({ text: action.payload, id: Date.now() });
+    },
+    remove: (state, action) =>
+      state.filter((toDo) => toDo.id !== action.payload),
   },
-  [deleteToDo]: (state, action) =>
-    state.filter((toDo) => toDo.id !== action.payload), // return new state
 });
 
 //* configureStore activates Redux Dev Tool
-const store = configureStore({ reducer });
+const store = configureStore({ reducer: toDos.reducer });
 
-export const actionCreators = {
-  addToDo,
-  deleteToDo,
-};
+//* createSlice also gives actions
+export const { add, remove } = toDos.actions;
+
+console.log(add);
 
 export default store;
+
+//todo Replaced by createAction
 
 // const ADD = 'ADD';
 // const DELETE = 'DELETE';
@@ -54,3 +55,18 @@ export default store;
 //       return state;
 //   }
 // };
+
+//todo Replaced by createSlice
+
+// const addToDo = createAction('ADD');
+// const deleteToDo = createAction('DELETE');
+
+// //todo now it's OK to mutate the state by 'immer'
+// //* 2 options: mutate the state OR return new state, but not both
+// const reducer = createReducer([], {
+//   [addToDo]: (state, action) => {
+//     state.push({ text: action.payload, id: Date.now() }); // mutate the state
+//   },
+//   [deleteToDo]: (state, action) =>
+//     state.filter((toDo) => toDo.id !== action.payload), // return new state
+// });
