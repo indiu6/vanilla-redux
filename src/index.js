bb55d0cdacc9a6e555800f5579683a9eac163cf9
indiu6 <hewas6@gmail.com>
2021-01-22 21:27:45 -0500

    NEVER MUTATE STATE

--- src/index.js
+++ src/index.js
@@ -1,37 +1,68 @@
 import { createStore } from 'redux';
 
-const add = document.getElementById('add');
-const minus = document.getElementById('minus');
-const number = document.querySelector('span');
+const form = document.querySelector('form');
+const input = document.querySelector('input');
+const ul = document.querySelector('ul');
 
-number.innerHTML = 0;
+const ADD_TODO = 'ADD_TODO';
+const DELETE_TODO = 'DELETE_TODO';
 
-const ADD = 'add';
-const MINUS = 'minus';
-
-const countModifier = (count = 0, action) => {
+const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
-      return count + 1;
-    case MINUS:
-      return count - 1;
+    case ADD_TODO:
+      return [];
+    case DELETE_TODO:
+      return [];
     default:
-      return count;
+      return state;
   }
 };
 
-const countStore = createStore(countModifier);
+const store = createStore(reducer);
 
-const onChange = () => {
-  number.innerHTML = countStore.getState();
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = '';
+  store.dispatch({ type: ADD_TODO, data: toDo });
 };
 
-countStore.subscribe(onChange);
+form.addEventListener('submit', onSubmit);
 
-add.addEventListener('click', () => {
-  countStore.dispatch({ type: ADD });
-});
+//todo COUNT
 
-minus.addEventListener('click', () => {
-  countStore.dispatch({ type: MINUS });
-});
+// const add = document.getElementById('add');
+// const minus = document.getElementById('minus');
+// const number = document.querySelector('span');
+
+// number.innerHTML = 0;
+
+// const ADD = 'add';
+// const MINUS = 'minus';
+
+// const countModifier = (count = 0, action) => {
+//   switch (action.type) {
+//     case ADD:
+//       return count + 1;
+//     case MINUS:
+//       return count - 1;
+//     default:
+//       return count;
+//   }
+// };
+
+// const countStore = createStore(countModifier);
+
+// const onChange = () => {
+//   number.innerHTML = countStore.getState();
+// };
+
+// countStore.subscribe(onChange);
+
+// add.addEventListener('click', () => {
+//   countStore.dispatch({ type: ADD });
+// });
+
+// minus.addEventListener('click', () => {
+//   countStore.dispatch({ type: MINUS });
+// });
